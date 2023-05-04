dfd903fb1c3e80efd350bfc5ac9147c1891e90cb
Casperlaiyuansoon <108987766+Casperlaiyuansoon@users.noreply.github.com>
2023-05-04 17:50:59 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cyq6ppvuh1ze.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "database-1"
+custombucket = "ikea-employee"
+customregion = "us-east-1"
+
